7fadcf67f25df431305b563ce6f7b63f400713ed
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-06-01 23:57:04 -0300

    finished home

--- fashionshop/views.py
+++ fashionshop/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def Home(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-id')
 
     return render(
         request, template_name="home.html", context={'products': products}
@@ -15,18 +15,25 @@ def Home(request):
 def get_by_name(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        products = Product.objects.filter(name__icontains=name)
+        products = Product.objects.filter(name__icontains=name).order_by('-id')
+
+        message = False
+
+        if not products.exists():
+            message = "Nenhum produto encontrado"
 
     return render(
-        request, template_name="home.html", 
-        context={'products': products}
+        request, template_name="home.html",
+        context={'products': products, 'message': message}
     )
 
 
 def get_by_categories(request):
     if request.method == 'POST':
         categorie = request.POST.get('categories')
-        products = Product.objects.filter(categories__name__icontains=categorie)
+        products = Product.objects.filter(
+            categories__name__icontains=categorie
+        ).order_by('-id')
 
     return render(
         request, template_name="home.html", 
